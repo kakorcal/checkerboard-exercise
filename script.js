d5db370b9ad1ac8fe282dd63573fd73d0c9f5502
Kenneth Korcal <kakorcal@gmail.com>
2016-02-29 18:13:12 -0800

    commit flashing

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 // Your JS goes here
+var tilearr = [];
 var body = document.getElementsByTagName('body')[0];
-var overlay = document.createElement('div');
-
 body.setAttribute('style', 'margin: 0; padding: 0;');
-overlay.setAttribute('style', 'background: linear-gradient(rgba(0,0,0,.5), rgba(255,255,255,.5)); position: absolute; top: 0; left: 0; width: 100%; height: 100%;')
 
 for(var i = 0; i < 45; i++){
   var tile = document.createElement('div');
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
   tile.setAttribute('style', 'width: 11.1%; padding-bottom: 11.1%; float: left;');
-  if(i % 2 === 0){
-    tile.style.backgroundColor = '#3dcafc';
-  }else{
-    tile.style.backgroundColor = '#c92dfa';
-  }
+  tile.style.backgroundColor = 'rgb(' + r + ',' + b + ',' + g + ')';
+  tilearr.push(tile);
   body.appendChild(tile);
 }
-body.appendChild(overlay);
+
+setInterval(function(){
+  tilearr.forEach(function(curr){
+    console.log(curr);
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+    curr.style.backgroundColor = 'rgb(' + r + ',' + b + ',' + g + ')';
+  });
+}, 2000);
