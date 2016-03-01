c6b33d9dd4a3241d3651f031882d9ab7ec106f8d
Kenneth Korcal <kakorcal@gmail.com>
2016-02-29 18:03:08 -0800

    commit random branch

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 // Your JS goes here
 var body = document.getElementsByTagName('body')[0];
-var overlay = document.createElement('div');
-
 body.setAttribute('style', 'margin: 0; padding: 0;');
 
 for(var i = 0; i < 45; i++){
   var tile = document.createElement('div');
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
   tile.setAttribute('style', 'width: 11.1%; padding-bottom: 11.1%; float: left;');
-  if(i % 2 === 0){
-    tile.style.backgroundColor = 'black';
-  }else{
-    tile.style.backgroundColor = 'red';
-  }
+  tile.style.backgroundColor = 'rgb(' + r + ',' + b + ',' + g + ')';
   body.appendChild(tile);
 }
