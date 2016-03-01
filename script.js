d0f8f88854e09355a70e562d17b86802edc47016
Kenneth Korcal <kakorcal@gmail.com>
2016-02-29 18:00:38 -0800

    commit master

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Your JS goes here
+var body = document.getElementsByTagName('body')[0];
+var overlay = document.createElement('div');
+
+body.setAttribute('style', 'margin: 0; padding: 0;');
+
+for(var i = 0; i < 45; i++){
+  var tile = document.createElement('div');
+  tile.setAttribute('style', 'width: 11.1%; padding-bottom: 11.1%; float: left;');
+  if(i % 2 === 0){
+    tile.style.backgroundColor = 'black';
+  }else{
+    tile.style.backgroundColor = 'red';
+  }
+  body.appendChild(tile);
+}
